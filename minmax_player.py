c6d7367eef7c2b1137fb5f5d2f1a57d815ce9062
Ruoxi Qin <qinruoxi@outlook.com>
2018-04-22 16:03:32 -0400

    add illegal check

--- minmax_player.py
+++ minmax_player.py
@@ -23,6 +23,8 @@ class MinmaxPlayer:
         self.empty_positions = deepcopy(empty_positions)
         score, color, position = self._compute_score(self.board, 
             self.last_move, self.empty_positions, 0, inf, -inf, True)
+        if position not in get_empty_positions(board):
+            assert(False, "I maxmin makes an illegal move!!!")
         return color, position, info
 
     def _compute_score(self, board, last_move, empty_positions, depth, 
